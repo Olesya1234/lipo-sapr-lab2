06d1d8a847d478eaea6fee6e1aa849e7fbfe5835
HOME <HOME@DNS>
2018-12-03 18:36:10 +0700

    lipo-sapr-lab2

---
+++ src/ErrorFinder/ErrorFinder.h
@@ -0,0 +1,16 @@
+class ErrorFinder
+
+{
+	
+public:
+	ErrorFinder();
+	
+int GetErrorsCount();
+		
+const char* mainRegexp;
+		
+static constexpr char* noteStart = "(Each undeclared identifier is reported only once";
+		
+static constexpr char* noteEnd = "for each function it appears in.)";
+
+};
